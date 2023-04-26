f0b0bbf04f7fe52182e84eee734c6b1971a44d5b
luka <lukabarbakadze18@gmail.com>
2023-04-26 19:52:28 +0400

    login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,38 @@ import { createUser, getUserByEmail } from "../db/users";
 import express from "express";
 import { authentication, random } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+    };
+
+    const expectedHash = authentication(user.authentication.salt, password);
+    if(user.authentication.password !== expectedHash) {
+      res.status(403).json({message: 'Incorrect password'});
+    };
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+    await user.save();
+    // res.cookie('AUTH', user.authentication.sessionToken, {domain: 'localhost', path: '/'});
+    res.cookie('AUTH', user.authentication.sessionToken);
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
@@ -12,7 +44,9 @@ export const register = async (req: express.Request, res: express.Response) => {
 
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
-        return res.status(400).json({ error: "User with this email already exists" });
+      return res
+        .status(400)
+        .json({ error: "User with this email already exists" });
     }
 
     const salt = random();
@@ -27,6 +61,8 @@ export const register = async (req: express.Request, res: express.Response) => {
     return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
-    return res.status(400).json({ error: "An error occurred while processing the request" });
+    return res
+      .status(400)
+      .json({ error: "An error occurred while processing the request" });
   }
 };
