1878b5caa8c546807761534a3955cc6e7d3dc5f7
luka <lukabarbakadze18@gmail.com>
2023-04-26 20:22:36 +0400

    finishing touches

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -16,13 +16,37 @@ export const isAuthenticated = async (
 
     const existingUser = await getUserBySessionToken(sessionToken);
     if (!existingUser) {
-      return res.status(403).json({ message: "User not found" });
-    };
+      return res.status(403).json({ message: "User not Authenticated" });
+    }
 
     merge(req, { identity: existingUser });
     return next();
   } catch (error) {
     console.log(error);
     return res.status(400);
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(400);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
   }
 };
